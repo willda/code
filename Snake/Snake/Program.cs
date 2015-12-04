979bd415a24bcdf99cad3f4e1aff97facdf5d61f
willda <willda@yandex.ru>
2015-12-04 18:31:56 +0500

    add list

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,6 +17,26 @@ namespace Snake
             point p2 = new point(3, 4, '#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add( 0 );
+            numList.Add( 1 );
+            numList.Add( 2 );
+
+            int x = numList[ 0 ];
+            int y = numList[ 1 ];
+            int z = numList[ 2 ];
+
+            foreach( int i in numList)
+            {
+                Console.WriteLine(1);
+            };
+
+            numList.RemoveAt(0);
+
+            List<point> pList = new List<point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
         static void Draw(int x, int y, char sym)
