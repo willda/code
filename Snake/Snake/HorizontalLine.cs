1457e2b032a1fab94a3aeda6eab902045bd2d52d
willda <willda@yandex.ru>
2015-12-04 19:02:09 +0500

    add new klass HorizontalLine

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<point> plist;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            plist = new List<point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                point p = new point(x, y, sym);
+                plist.Add(p);
+            }
+
+            
+        }
+
+        public void Draw()
+        {
+            foreach (point p in plist)
+            {
+                p.Draw();
+            }
+
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,32 +17,11 @@ namespace Snake
             point p2 = new point(3, 4, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add( 0 );
-            numList.Add( 1 );
-            numList.Add( 2 );
-
-            int x = numList[ 0 ];
-            int y = numList[ 1 ];
-            int z = numList[ 2 ];
-
-            foreach( int i in numList)
-            {
-                Console.WriteLine(1);
-            };
-
-            numList.RemoveAt(0);
-
-            List<point> pList = new List<point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
 
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+   
     }
 }
