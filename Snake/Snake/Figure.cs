bd777582ccf7133c511c5a46795ad85f1dc5aae9
willda <willda@yandex.ru>
2015-12-05 18:45:00 +0500

    create class Figure

--- Snake/Snake/Figure.cs
+++ Snake/Snake/Figure.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +7,13 @@ namespace Snake
 {
     class Figure
     {
+        protected List<point> plist;
+        public void Draw()
+        {
+            foreach (point p in plist)
+            {
+                p.Draw();
+            }
+        }
     }
 }
---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake:Figure
+    {
+        public Snake(point tail, int length, Direction direction)
+        {
+            plist = new List<point>();
+            for (int i = 0; i<length; i++)
+            {
+                point p = new point(tail);
+                p.Move(i, direction);
+                plist.Add(p);
+
+            }
+        }
+    }
+}
