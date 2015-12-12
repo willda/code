3bbb4a8e8c162b8d2be3ae30bfcbccd30d8080d0
willda <willda@yandex.ru>
2015-12-12 19:57:51 +0500

    change

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,6 +13,14 @@ namespace Snake
 
             Console.SetBufferSize(80, 25);
 
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '-');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 23, '-');
+            VerticaleLine leftLine = new VerticaleLine(0, 24, 0, '-');
+            VerticaleLine rightLine = new VerticaleLine(0, 24, 78, '-');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Drow();
+            rightLine.Drow();
 
             point p = new point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
